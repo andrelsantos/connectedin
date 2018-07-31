17736a4a95c138e5e6ef4cc20df3ead2b82adbfa
andrelsantos <andre.lsantos.info@gmail.com>
2018-07-31 19:43:57 -0300

    Commit inicial

---
+++ perfis/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def exibir(request):
+    return render(request, 'perfil.html')
+
+# Create your views here.
